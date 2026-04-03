0c28404ee28e2fbb52c40c70340de289b8e073a8
Jaiminkumar Patel <cap.jemspatel1212@gmail.com>
2026-04-03 16:32:53 +0530

    Feature | Bug_fix_Forgot password added

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'bill_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,12 +13,178 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  @override
+  @override
+  void initState() {
+    super.initState();
+
+    _loadSavedPassword();
+
+    _usernameController.addListener(() {
+      setState(() {
+        _errorMessage = '';
+      });
+    });
+
+    _passwordController.addListener(() {
+      setState(() {
+        _errorMessage = '';
+      });
+    });
+  }
+
+  String _savedPassword = '1234';
+
+  Future<void> _loadSavedPassword() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _savedPassword = prefs.getString('password') ?? '1234';
+    });
+  }
+
+  Future<void> _saveNewPassword(String newPassword) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('password', newPassword);
+
+    setState(() {
+      _savedPassword = newPassword;
+      _passwordController.text = newPassword;
+    });
+  }
+
+  void _showForgotPasswordDialog() {
+    FocusScope.of(context).unfocus();
+
+    final keyController = TextEditingController();
+    final usernameController = TextEditingController();
+    final newPasswordController = TextEditingController();
+    final confirmPasswordController = TextEditingController();
+
+    final FocusNode keyFocus = FocusNode();
+
+    showDialog(
+      context: context,
+      useRootNavigator: true,
+      barrierDismissible: false,
+      builder: (context) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          keyFocus.requestFocus();
+        });
+
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Focus(
+            autofocus: true,
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    'Reset Password',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(height: 20),
+
+                  _buildField(
+                    'Enter Key',
+                    keyController,
+                    false,
+                    keyFocus,
+                    true,
+                  ),
+                  const SizedBox(height: 12),
+
+                  _buildField('Username', usernameController),
+                  const SizedBox(height: 12),
+
+                  _buildField('New Password', newPasswordController, true),
+                  const SizedBox(height: 12),
+
+                  _buildField(
+                    'Confirm Password',
+                    confirmPasswordController,
+                    true,
+                  ),
+                  const SizedBox(height: 20),
+
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: const Text('Cancel'),
+                        ),
+                      ),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // same logic
+                          },
+                          child: const Text('Submit'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildField(
+    String label,
+    TextEditingController controller, [
+    bool isPassword = false,
+    FocusNode? focusNode,
+    bool autoFocus = false,
+  ]) {
+    return TextField(
+      controller: controller,
+      focusNode: focusNode,
+      autofocus: autoFocus,
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        labelText: label,
+        filled: true,
+        fillColor: Colors.grey.shade100,
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 12,
+          vertical: 14,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(color: Color(0xFFFF7A18)),
+        ),
+      ),
+    );
+  }
+
+  void _showMessage(String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
+  }
+
+  final FocusNode keyFocus = FocusNode();
+
   final _formKey = GlobalKey<FormState>();
   bool _isLoading = false;
   String _errorMessage = '';
   bool _obscurePassword = true;
+  bool get isEnabled =>
+      _usernameController.text.trim().isNotEmpty &&
+      _passwordController.text.trim().isNotEmpty &&
+      !_isLoading;
 
-  void _login() {
+  void _login() async {
     if (!_formKey.currentState!.validate()) return;
 
     setState(() {
@@ -25,13 +192,16 @@ class _LoginScreenState extends State<LoginScreen> {
       _errorMessage = '';
     });
 
+    final prefs = await SharedPreferences.getInstance();
+    String currentPassword = prefs.getString('password') ?? '1234';
+
     Future.delayed(const Duration(seconds: 1), () {
       setState(() {
         _isLoading = false;
       });
 
       if (_usernameController.text == 'admin' &&
-          _passwordController.text == '1234') {
+          _passwordController.text == currentPassword) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (_) => const BillScreen()),
@@ -73,10 +243,12 @@ class _LoginScreenState extends State<LoginScreen> {
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     const Text(
-                      'Billing App Login',
+                      'Aksh Enterprises',
                       style: TextStyle(
-                        fontSize: 22,
+                        fontSize: 24,
                         fontWeight: FontWeight.bold,
+                        letterSpacing: 0.5,
+                        color: Color(0xFF2C2C2C),
                       ),
                     ),
                     const SizedBox(height: 24),
@@ -105,16 +277,17 @@ class _LoginScreenState extends State<LoginScreen> {
                         labelText: 'Password',
                         border: const OutlineInputBorder(),
                         suffixIcon: IconButton(
-                          icon: Icon(
-                            _obscurePassword
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                          ),
                           onPressed: () {
                             setState(() {
                               _obscurePassword = !_obscurePassword;
                             });
                           },
+                          icon: Icon(
+                            _obscurePassword
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                            color: Colors.grey.shade600,
+                          ),
                         ),
                       ),
                       validator: (value) {
@@ -125,10 +298,23 @@ class _LoginScreenState extends State<LoginScreen> {
                       },
                     ),
 
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: _showForgotPasswordDialog,
+                        child: const Text(
+                          'Forgot Password?',
+                          style: TextStyle(fontSize: 13, color: Colors.black54),
+                        ),
+                      ),
+                    ),
+
                     const SizedBox(height: 20),
 
                     /// Error message
-                    if (_errorMessage.isNotEmpty)
+                    if (_errorMessage.isNotEmpty &&
+                        _usernameController.text.isNotEmpty &&
+                        _passwordController.text.isNotEmpty)
                       Text(
                         _errorMessage,
                         style: const TextStyle(color: Colors.red),
@@ -139,17 +325,66 @@ class _LoginScreenState extends State<LoginScreen> {
                     /// Login Button
                     SizedBox(
                       width: double.infinity,
-                      height: 45,
-                      child: ElevatedButton(
-                        onPressed: _isLoading ? null : _login,
-                        child: _isLoading
-                            ? const CircularProgressIndicator(
-                                color: Colors.white,
-                              )
-                            : const Text(
-                                'LOGIN',
-                                style: TextStyle(fontSize: 16),
+                      height: 50,
+                      child: Builder(
+                        builder: (context) {
+                          bool isEnabled =
+                              _usernameController.text.trim().isNotEmpty &&
+                              _passwordController.text.trim().isNotEmpty &&
+                              !_isLoading;
+
+                          return ElevatedButton(
+                            onPressed: isEnabled ? _login : null,
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              backgroundColor: Colors.transparent,
+                              shadowColor: Colors.transparent,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
                               ),
+                              padding: EdgeInsets.zero,
+                            ),
+                            child: Ink(
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: isEnabled
+                                      ? [
+                                          const Color(0xFFFF7A18),
+                                          const Color(0xFFFFA726),
+                                        ]
+                                      : [
+                                          Colors.grey.shade400,
+                                          Colors.grey.shade400,
+                                        ],
+                                ),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Container(
+                                alignment: Alignment.center,
+                                child: _isLoading
+                                    ? const SizedBox(
+                                        height: 22,
+                                        width: 22,
+                                        child: CircularProgressIndicator(
+                                          strokeWidth: 2.5,
+                                          color: Colors.white,
+                                        ),
+                                      )
+                                    : Text(
+                                        'LOGIN',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600,
+                                          letterSpacing: 1,
+                                          color: isEnabled
+                                              ? Colors.white
+                                              : Colors.white70,
+                                        ),
+                                      ),
+                              ),
+                            ),
+                          );
+                        },
                       ),
                     ),
                   ],
